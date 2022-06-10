4710d724bbee048ea15758c6d475e6219f971d68
ivan <example@gmail.com>
2022-06-10 09:59:19 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+import {one as first} from './main.js';
+import {two as second} from './main.js';
+import sayHi from './main.js'; // {default as sayHi}
+console.log(first);
+console.log(second);
+sayHi();
